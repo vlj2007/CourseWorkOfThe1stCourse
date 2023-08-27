b7f6a49d197306d3d63a67104c99a2df9b08de14
vlj2007 <vlj2007@yandex.ru>
2023-08-27 23:50:58 +0700

    I have finished my course work.

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,29 @@ public class Main {
     public static void main(String[] args) {
         myApp();
     }
+
     public static void myApp() {
         Employee[] employees = new Employee[10];
-        employees[0] = new Employee("Musk Elon Reeve", "1", 1_000_456.00);
-        employees[1] = new Employee("Spears Britney Jean", "2", 4_500.00);
-        employees[2] = new Employee("Mapother IV Thomas Cruise", "1", 4_500.00);
-        employees[3] = new Employee("Voight Angelina Jolie", "4", 44_500.00);
-        employees[4] = new Employee("Charles Keanu Reeves", "5", 500.00);
+        employees[0] = new Employee("Musk Elon Reeve", "1", 4000.00);
+        employees[1] = new Employee("Spears Britney Jean", "2", 4000.00);
+        employees[2] = new Employee("Mapother IV Thomas Cruise", "1", 4000.00);
+        employees[3] = new Employee("Voight Angelina Jolie", "4", 4000.00);
+        employees[4] = new Employee("Charles Keanu Reeves", "5", 4000.00);
         employees[5] = new Employee("Wilhelm Leonardo DiCaprio", "3", 488_500.00);
         employees[6] = new Employee("Laura Winona Horowitz", "2", 4500.00);
-        employees[7] = new Employee("Spears Britney Jean", "2", 4577.00);
-        employees[8] = new Employee("Spears Britney Jean", "1", 4500.00);
-        employees[8].setSalary(600.00);
+        employees[7] = new Employee("Spears Britney Jean", "2", 4000.00);
+        employees[8] = new Employee("Spears Britney Jean", "1", 4000.00);
+        employees[8].setSalary(2000.00);
         employees[8].setDepartment("4");
-        employees[9] = new Employee("Spears Britney Jean", "1", 4500.00);
-        //отображаем массив
+        employees[9] = new Employee("Spears Britney Jean", "1", 4000.00);
         Employee.showEmployee(employees);
-        //Отображаем сумму зарплат
         Employee.sumSalary(employees);
         Employee.mediumSalary(employees);
         Employee.minSalary(employees);
         Employee.maxSalary(employees);
         System.out.println("Сумму затрат на зарплаты в месяц -- " + Employee.sumSalary(employees));
-        System.out.println("Cреднее значение зарплат -- " + " " + Employee.mediumSalary(employees));
-        System.out.println("Cотрудник с минимальной зарплатой -- "+ Employee.minSalary(employees));
+        System.out.println("Cреднее значение зарплат -- " + Employee.mediumSalary(employees));
+        System.out.println("Cотрудник с минимальной зарплатой -- " + Employee.minSalary(employees));
         System.out.println("Cотрудника с максимальной зарплатой -- " + Employee.maxSalary(employees));
     }
 }
--- src/pro/sky/java/course1/CourseWork/Employee.java
+++ src/pro/sky/java/course1/CourseWork/Employee.java
@@ -1,11 +1,14 @@
 package pro.sky.java.course1.CourseWork;
 
+import java.util.Objects;
+
 public class Employee {
     private static int count;
     private final int id;
-    public String fullName;
-    public String department;
-    public double salary;
+    private String fullName;
+    private String department;
+    private double salary;
+
     public Employee(String fullName, String department, double salary) {
         count++;
         this.id = count;
@@ -13,65 +16,111 @@ public class Employee {
         this.department = department;
         this.salary = salary;
     }
+
     public static int getCount() {
         return count;
     }
+
     public int getId() {
         return id;
     }
+
     public String getFullName() {
         return fullName;
     }
+
     public String getDepartment() {
         return department;
     }
+
     public double getSalary() {
         return salary;
     }
+
     public void setSalary(double salary) {
         this.salary = salary;
     }
+
     public void setDepartment(String department) {
         this.department = department;
     }
+
     public String toString() {
         return this.fullName;
     }
+
+    /**
+     * @param employees Метод showEmployee выводит список всех сотрудников.
+     */
     public static void showEmployee(Employee[] employees) {
         for (int i = 0; i < employees.length; i++) {
-            if(employees[i] != null){
+            if (employees[i] != null) {
                 System.out.println(employees[i].getId() + "\t " + employees[i].getFullName() + "\t " + employees[i].getDepartment() + "\t " + employees[i].getSalary());
             }
         }
     }
-    public static double sumSalary(Employee[] employees){
+
+    /**
+     * @param employees Метод sumSalary возвращает сумму затрат на ЗП в месяц.
+     * @return
+     */
+    public static double sumSalary(Employee[] employees) {
         double sumSalary = 0;
-        for ( Employee employee : employees) {
-            if(employee!=null){
+        for (Employee employee : employees) {
+            if (employee != null) {
                 sumSalary += employee.getSalary();
             }
         }
         return sumSalary;
     }
-    public static double mediumSalary(Employee[] employees) {
-        return sumSalary(employees) / 10;
+
+    /**
+     * @param employees Метод numberOfNonNullArrayValues проходит по массиву сотрудников и возвращает не null записи.
+     * @return
+     */
+    public static int numberOfNonNullArrayValues(Employee[] employees) {
+        int notNull = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                notNull++;
+            }
+        }
+        return notNull;
     }
-    public static double minSalary(Employee[] employees){
+
+    /**
+     * @param employees Метод mediumSalary возвращает сред. зарплаты.
+     * @return
+     */
+    public static double mediumSalary(Employee[] employees) {
+        return sumSalary(employees) / numberOfNonNullArrayValues(employees);
+    }
+
+    /**
+     * @param employees Метод minSalary возвращает минимальную зарплату
+     * @return
+     */
+    public static double minSalary(Employee[] employees) {
         double minimumSalary = employees[0].salary;
-        for ( Employee employee : employees) {
-            if(employee!=null){
-                if(minimumSalary > employee.getSalary()){
+        for (Employee employee : employees) {
+            if (employee != null) {
+                if (minimumSalary > employee.getSalary()) {
                     minimumSalary = employee.getSalary();
                 }
             }
         }
         return minimumSalary;
     }
-    public static double maxSalary(Employee[] employees){
+
+    /**
+     * @param employees Метод maxSalary возвращает максимальныю зарплату
+     * @return
+     */
+    public static double maxSalary(Employee[] employees) {
         double maximumSalary = employees[0].salary;
-        for ( Employee employee : employees) {
-            if(employee!=null){
-                if(maximumSalary < employee.getSalary()){
+        for (Employee employee : employees) {
+            if (employee != null) {
+                if (maximumSalary < employee.getSalary()) {
                     maximumSalary = employee.getSalary();
                 }
             }
